0e5314661417b6dc43b7d893162cad6869d9d31c
qyin7 <quanjunyin@gmail.com>
2014-04-19 22:08:24 -0400

    Update grabber.php

--- grabber.php
+++ grabber.php
@@ -1,32 +1,32 @@
 function get_naps_bot()
 {
     $useragent = strtolower($_SERVER['HTTP_USER_AGENT']);
-    if (strpos($useragent, ‘googlebot’) !== false){
-    return ‘Googlebot’;
+    if (strpos($useragent, 'googlebot') !== false){
+    return 'Googlebot';
     }
-    if (strpos($useragent, ‘msnbot’) !== false){
-    return ‘MSNbot’;
+    if (strpos($useragent, 'msnbot') !== false){
+    return 'MSNbot';
     }
-    if (strpos($useragent, ’slurp’) !== false){
-    return ‘Yahoobot’;
+    if (strpos($useragent, 'slurp') !== false){
+    return 'Yahoobot';
     }
-    if (strpos($useragent, ‘baiduspider’) !== false){
-    return ‘Baiduspider’;
+    if (strpos($useragent, 'baiduspider') !== false){
+    return 'Baiduspider';
     }
-    if (strpos($useragent, ’sohu-search’) !== false){
-    return ‘Sohubot’;
+    if (strpos($useragent, 'sohu-search') !== false){
+    return 'Sohubot';
     }
-    if (strpos($useragent, ‘lycos’) !== false){
-    return ‘Lycos’;
+    if (strpos($useragent, 'lycos') !== false){
+    return 'Lycos';
     }
-    if (strpos($useragent, ‘robozilla’) !== false){
-    return ‘Robozilla’;
+    if (strpos($useragent, 'robozilla') !== false){
+    return 'Robozilla';
     }
     return false;
 }
 
 function nowtime(){
-    $date=date(”Y-m-d.G:i:s”);
+    $date=date('Y-m-d.G:i:s');
     return $date;
 }
 
@@ -34,9 +34,9 @@ $searchbot = get_naps_bot();
 if ($searchbot) {
     $tlc_thispage = addslashes($_SERVER['HTTP_USER_AGENT']);
     $url=$_SERVER['HTTP_REFERER'];
-    $file=”robotlog.txt”;
+    $file='robotlog.txt';
     $time=nowtime();
-    $data=fopen($file,”a”);
-    fwrite($data,”Time:$time robot:$searchbot URL:$tlc_thispage “);
+    $data=fopen($file,'a');
+    fwrite($data,'Time:$time robot:$searchbot URL:$tlc_thispage ');
     fclose($data);
 }
